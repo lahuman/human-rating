176e9868c327c38e321968b8f22f947b871d1ff5
Daniel Lim <lahuman@daum.net>
2023-09-19 17:43:53 +0900

    feat : 평가 기능 개발 중

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -19,7 +19,7 @@ export default function Login() {
     if (pb.authStore.isValid) {
       router.push("/");
     }
-  }, [router]);
+  }, [pb.authStore]);
   return (
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8 bg-white">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -87,7 +87,6 @@ export default function Login() {
 
           <div>
             <button
-              type="submit"
               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               onClick={(e) => {
                 if (login) login(loginInfo.id, loginInfo.pw);
--- app/pb.ts
+++ app/pb.ts
@@ -1,5 +1,6 @@
 import PocketBase from "pocketbase";
 
 const pb = new PocketBase("https://lahuman.fly.dev");
+pb.autoCancellation(false);
 
 export default pb;
