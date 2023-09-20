ac4c36824bd75ddb6c700c64b6d28a380478558d
Daniel Lim <lahuman@daum.net>
2023-09-20 17:34:20 +0900

    fix : build 수정

---
+++ app/human/rating.ts
@@ -0,0 +1,8 @@
+export const ratingList = [
+    { "key": "apperance", name: '외형' },
+    { "key": "interpersonal", name: '대인관계' },
+    { "key": "communication", name: '소통' },
+    { "key": "proferssional", name: '업무' },
+    { "key": "ethical", name: '도덕성' },
+    { "key": "problem_solve", name: '문제해결' }
+  ];
