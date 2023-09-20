3bac662905298b785738105c9dd71d4322d3d8ac
K <k@Kui-iMac.local>
2023-09-20 09:27:55 +0900

    fix: 로그인시 post form 제거 처리

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -36,66 +36,66 @@ export default function Login() {
       </div>
 
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form className="space-y-6" action="#" method="POST">
-          <div>
+
+        <div>
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Email address
+          </label>
+          <div className="mt-2">
+            <input
+              id="email"
+              name="email"
+              type="email"
+              autoComplete="email"
+              required
+              className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              value={loginInfo.id}
+              onChange={(e) =>
+                setLoginInfo({ ...loginInfo, id: e.target.value })
+              }
+            />
+          </div>
+        </div>
+
+        <div>
+          <div className="flex items-center justify-between">
             <label
-              htmlFor="email"
+              htmlFor="password"
               className="block text-sm font-medium leading-6 text-gray-900"
             >
-              Email address
+              Password
             </label>
-            <div className="mt-2">
-              <input
-                id="email"
-                name="email"
-                type="email"
-                autoComplete="email"
-                required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                value={loginInfo.id}
-                onChange={(e) =>
-                  setLoginInfo({ ...loginInfo, id: e.target.value })
-                }
-              />
-            </div>
           </div>
+          <div className="mt-2">
+            <input
+              id="password"
+              name="password"
+              type="password"
+              autoComplete="current-password"
+              required
+              className="pl-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+              value={loginInfo.pw}
+              onChange={(e) =>
+                setLoginInfo({ ...loginInfo, pw: e.target.value })
+              }
+            />
+          </div>
+        </div>
 
-          <div>
-            <div className="flex items-center justify-between">
-              <label
-                htmlFor="password"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Password
-              </label>
-            </div>
-            <div className="mt-2">
-              <input
-                id="password"
-                name="password"
-                type="password"
-                autoComplete="current-password"
-                required
-                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                value={loginInfo.pw}
-                onChange={(e) =>
-                  setLoginInfo({ ...loginInfo, pw: e.target.value })
-                }
-              />
-            </div>
-          </div>
-
-          <div>
-            <button
-              className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              onClick={(e) => {
-                if (login) login(loginInfo.id, loginInfo.pw);
-              }}
-            >
-              Sign in
-            </button>
-          </div>
-        </form>
+        <div>
+          <button
+            type="button"
+            className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            onClick={(e) => {
+              if (login) login(loginInfo.id, loginInfo.pw);
+            }}
+          >
+            Sign in
+          </button>
+        </div>
       </div>
     </div>
   );
