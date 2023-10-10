8c0ba35e782a6a12af2e5fb59ff7ee504a617ec1
K <k@Kui-iMac.local>
2023-10-11 08:56:10 +0900

    feat : remove cache

--- app/api/holidays/[year]/route.ts
+++ app/api/holidays/[year]/route.ts
@@ -4,9 +4,16 @@ export async function GET(
   request: NextRequest,
   { params }: { params: { year: string } }
 ) {
+
+  const year = parseInt(params.year.substring(0, 4), 10);
+  if (year < 1900 || year > 2100) {
+    return Response.json({ error: true, message: "Invalid year. Please use a year between 1900 and 2100." });
+  }
+  
   const holidayList = await fetch(
     `https://lahuman.fly.dev/api/collections/holiday/records?perPage=500&filter=(year=${params.year})`,
     {
+      cache: 'no-store',
       headers: {
         x_token: process.env.TOKEN as string,
       },
